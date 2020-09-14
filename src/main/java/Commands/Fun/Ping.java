585e2c9b8fb1ddda147f8066676acd70e57779c8
Tais993 <49957334+Tais993@users.noreply.github.com>
2020-09-14 08:42:42 +0200

    Add files via upload
    8ball is cool

--- src/main/java/Commands/Fun/Ping.java
+++ src/main/java/Commands/Fun/Ping.java
@@ -4,23 +4,24 @@ import Commands.ICommand;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class Ping implements ICommand {
-
-    GuildMessageReceivedEvent e;
     String command = "ping";
+    String commandAlias = "pong";
     String category = "fun";
     String exampleCommand = "`!ping`";
     String shortCommandDescription = "Get a real ping";
     String fullCommandDescription = "Not much to say, just a command that gives your totally not fake ping.";
 
     public void command(GuildMessageReceivedEvent event, String[] args) {
-        e = event;
-        int ping = 1;
-
-        e.getChannel().sendMessage("Ping? " + ping + "ms").queue();
+        event.getChannel().sendMessage("Ping: " + event.getMember().getJDA().getGatewayPing() + "ms").queue();
     }
 
     public String getCommand() {
         return command;
+    }
+
+    @Override
+    public String getCommandAlias() {
+        return commandAlias;
     }
 
     @Override
